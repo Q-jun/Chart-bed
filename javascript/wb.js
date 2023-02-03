10c6defbe54c23ec28fb9a8993b6f8070a076bfb
Qn_JnB <84129138+Q-jun@users.noreply.github.com>
2023-02-03 10:49:34 +0800

    Update wb.js

--- javascript/wb.js
+++ javascript/wb.js
@@ -1,7 +1,6 @@
 $(function () {
   function get() {
-    $.get('https://api.vvhan.com/api/wbhot', function (res) {
-      console.log('res', res)
+    $.get('https://api.vvhan.com/api/hotlist?type=wbHot', function (res) {
       var htmlStr = template('tpl-weibo', res)
       $('#container').html(htmlStr)
     })
