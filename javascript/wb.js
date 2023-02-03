063f362b96c8634418a25912cf97bd22f3233f53
Qn_JnB <84129138+Q-jun@users.noreply.github.com>
2023-02-03 11:00:14 +0800

    Update wb.js

--- javascript/wb.js
+++ javascript/wb.js
@@ -1,9 +1,9 @@
-$(function () {
-  function get() {
-    $.get('https://api.vvhan.com/api/hotlist?type=wbHot', function (res) {
-      var htmlStr = template('tpl-weibo', res)
-      $('#container').html(htmlStr)
+ $(function () {
+      function get() {
+        $.get('https://api.vvhan.com/api/hotlist?type=wbHot', function (res) {
+          var htmlStr = template('tpl-weibo', res)
+          $('#container').html(htmlStr)
+        })  
+      }
+      get()
     })
-  }
-  get()
-})
